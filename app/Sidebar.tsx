c6f7575ba3d11391474c85c7199e1d46dc98c254
Kevin Tatooles <tatooles@wisc.edu>
2023-03-20 22:47:20 -0500

    Add animaton and button to open

--- app/Sidebar.tsx
+++ app/Sidebar.tsx
@@ -3,14 +3,12 @@ import { useState } from "react";
 
 export default function Sidebar({ isOpen }: any) {
   return (
-    <>
-      {isOpen ? (
-        <div className="fixed top-0 left-0 z-10 h-full w-40 bg-blue-500">
-          <h2 className="text-2xl text-white">This is the sidebar</h2>
-        </div>
-      ) : (
-        <></>
-      )}
-    </>
+    <div
+      className={`fixed top-0 left-0 z-10 h-full w-40 bg-blue-500 ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      } duration-300 ease-in-out `}
+    >
+      <h2 className="text-2xl text-white">This is the sidebar</h2>
+    </div>
   );
 }
