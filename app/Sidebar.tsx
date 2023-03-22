208efd4b0b1d5fc8dd425ffcdf27fe0e24af8ca2
Kevin Tatooles <tatooles@wisc.edu>
2023-03-21 22:29:17 -0500

    Style sidebar

--- app/Sidebar.tsx
+++ app/Sidebar.tsx
@@ -1,14 +1,27 @@
 "use client";
-import { useState } from "react";
+import Link from "next/link";
 
 export default function Sidebar({ isOpen }: any) {
   return (
     <div
-      className={`fixed top-0 left-0 z-20 h-full w-40 bg-blue-500 ${
+      className={`fixed top-0 left-0 z-20 h-full w-40 bg-gray-300 ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       } duration-300 ease-in-out `}
     >
-      <h2 className="text-2xl text-white">This is the sidebar</h2>
+      <ul className="p-5">
+        <li className="mb-5 text-xl">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="mb-5 text-xl">
+          <Link href="/">Workouts</Link>
+        </li>
+        <li className="mb-5 text-xl">
+          <Link href="/">Records</Link>
+        </li>
+        <li className="mb-5 text-xl">
+          <Link href="/">Settings</Link>
+        </li>
+      </ul>
     </div>
   );
 }
