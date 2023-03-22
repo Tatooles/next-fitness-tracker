44f17ecb67035eaff7757d2c0c51327cec11b5c8
Kevin Tatooles <tatooles@wisc.edu>
2023-03-21 22:21:50 -0500

    Fix sidebar logic

--- app/Sidebar.tsx
+++ app/Sidebar.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 export default function Sidebar({ isOpen }: any) {
   return (
     <div
-      className={`fixed top-0 left-0 z-10 h-full w-40 bg-blue-500 ${
+      className={`fixed top-0 left-0 z-20 h-full w-40 bg-blue-500 ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       } duration-300 ease-in-out `}
     >
--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,12 @@ export default function RootLayout({
     <html lang="en">
       <body className="container mx-auto flex h-screen flex-col bg-[#171717]">
         <Sidebar isOpen={sidebarOpen} />
+        <div
+          onClick={() => setSidebarOpen(false)}
+          className={`fixed top-0 bottom-0 left-0 right-0 z-10 bg-black bg-opacity-50 ${
+            sidebarOpen ? "" : "hidden"
+          }`}
+        ></div>
         <header>
           <nav className="flex justify-between p-5 text-lg text-white">
             <div onClick={() => setSidebarOpen(true)}>Menu</div>
