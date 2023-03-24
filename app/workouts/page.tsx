84ecb89653984c5a6380975156ac985d777111dd
Kevin Tatooles <tatooles@wisc.edu>
2023-03-23 22:41:34 -0500

    Display workouts in a list

--- app/workouts/page.tsx
+++ app/workouts/page.tsx
@@ -4,11 +4,8 @@ import Modal from "../Modal";
 export default function Home() {
   const [addWorkoutModalOpen, setAddWorkoutModalOpen] = useState(false);
 
-  // This is our full list of workouts
   const [workouts, setWorkouts] = useState<Object[]>([]);
   const [formData, setFormData] = useState({});
-
-  // One useState for each field in the input modal
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -22,15 +19,19 @@ export default function Home() {
       [event.target.name]: event.target.value,
     });
   };
-  /**
-   * TODO: Start fleshing out the workouts page
-   * For now just store the workouts in state, store in the db later
-   * Create a modal to enter the info
-   * Display the list of workouts
-   */
+
   return (
     <div className="p-5 text-center">
       <h1 className="mb-5 text-3xl">Workouts</h1>
+      <ul>
+        {workouts.map((workout: any, index) => (
+          <li key={index} className="mb-2 border-2 border-black">
+            <h3 className="text-xl">{workout.date}</h3>
+            <p>{workout.name}</p>
+            <p>{workout.notes}</p>
+          </li>
+        ))}
+      </ul>
       <button
         onClick={() => setAddWorkoutModalOpen(true)}
         className="rounded-md bg-gray-500 p-2 text-white"
@@ -41,9 +42,9 @@ export default function Home() {
         isOpen={addWorkoutModalOpen}
         handleClose={() => setAddWorkoutModalOpen(false)}
       >
-        <div className="fixed top-20 left-1/2 z-10 w-4/5 translate-x-[-50%] translate-y-[-10%] rounded-lg bg-white p-5 text-center">
+        <div className="fixed top-20 left-1/2 z-10 w-4/5 translate-x-[-50%] translate-y-[-10%] rounded-lg bg-white p-5">
           <h2 className="text-3xl">Add Workout</h2>
-          <form onSubmit={handleSubmit} className="flex flex-col items-start">
+          <form onSubmit={handleSubmit} className="flex flex-col">
             <label htmlFor="date">Date:</label>
             <input
               className="mb-5 bg-gray-200"
@@ -60,13 +61,14 @@ export default function Home() {
               name="name"
               onChange={handleChange}
             />
+            {/* TODO: Allow the user to add exercises */}
             <label htmlFor="notes">Notes:</label>
             <textarea
               className="mb-5 bg-gray-200"
               name="notes"
               id="notes"
-              cols={30}
-              rows={10}
+              cols={10}
+              rows={5}
               onChange={handleChange}
             ></textarea>
             <button className="self-center rounded-md bg-blue-400 p-2 text-white">
