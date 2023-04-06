937535fc158314f1dfe208ebb8e72da3aa2ee87d
Kevin Tatooles <tatooles@wisc.edu>
2023-04-05 23:01:22 -0500

    Display all exercise information

--- app/workouts/Workouts.tsx
+++ app/workouts/Workouts.tsx
@@ -10,14 +10,24 @@ export default function Workouts({ workouts }: { workouts: Workout[] }) {
       {workouts.map((workout: Workout, index) => (
         // TODO: Display exercises and sets for each workout, will need nested loops
         // TODO: Break exercise into it's own component inside this file
-        <li key={index} className="mb-2 border-2 border-black">
+        <li key={index} className="mb-2 border-2 border-black p-2">
           <h3 className="text-xl">{getDate(workout.date)}</h3>
           <p>{workout.name}</p>
-          {workout.exercises.map((exercise: Exercise, index) => (
-            <div key={index}>
-              <h3>{exercise.name}</h3>
-              <div>Sets will go here</div>
-              <p>Notes: {exercise.notes}</p>
+          {workout.exercises.map((exercise: Exercise, index2) => (
+            <div
+              className="mb-2 border-2 border-black p-2 text-left"
+              key={index2}
+            >
+              <>
+                <h3>{exercise.name}</h3>
+                {exercise.sets.map((set: Set, index3) => (
+                  <div key={index3} className="flex justify-around">
+                    <div>Reps: {set.reps}</div>
+                    <div>Weight: {set.weight}</div>
+                  </div>
+                ))}
+                <p>Notes: {exercise.notes}</p>
+              </>
             </div>
           ))}
         </li>
