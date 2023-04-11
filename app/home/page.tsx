e73b4d5ac90979645b05bbec41850fc4c5373ebf
Kevin Tatooles <tatooles@wisc.edu>
2023-04-10 23:11:58 -0500

    Add links and move padding into buttons

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="">This is the home page!!!</div>;
+  return (
+    <div className="text-center">
+      <h1 className="mt-4 text-2xl">Recent Trends</h1>
+    </div>
+  );
 }
