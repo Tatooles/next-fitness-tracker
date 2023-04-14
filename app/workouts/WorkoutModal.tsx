5dfb056198e4db150c7c1b387e58b83bdf331995
Kevin Tatooles <tatooles@wisc.edu>
2023-04-13 22:12:18 -0500

    Cleanup whitespace, comments

--- app/workouts/WorkoutModal.tsx
+++ app/workouts/WorkoutModal.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Modal from "../Modal";
-import { Workout, Exercise } from "./page";
+import { Workout } from "./page";
 
 export default function WorkoutModal({
   currentWorkouts,
@@ -19,6 +19,7 @@ export default function WorkoutModal({
   currentWorkoutIndex: number;
   setCurrentWorkoutIndex: React.Dispatch<React.SetStateAction<number>>;
 }) {
+  // This state holds the current data in the form
   const [formData, setFormData] = useState<Workout>({
     id: 0,
     date: "",
@@ -39,10 +40,7 @@ export default function WorkoutModal({
   useEffect(() => {
     if (editWorkoutId != -1) {
       // Pre fill
-      let workout = currentWorkouts[editWorkoutId];
-      console.log("prefilling modal");
-      // setFormData(currentWorkouts[editWorkoutId]);
-      setFormData(workout);
+      setFormData(currentWorkouts[editWorkoutId]);
     } else {
       setFormData({
         id: 0,
@@ -81,7 +79,7 @@ export default function WorkoutModal({
     setModalOpen(false);
   };
 
-  const handleChange = (event: any) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
       ...formData,
       [event.target.name]: event.target.value,
@@ -150,102 +148,100 @@ export default function WorkoutModal({
     });
     setFormData(data);
   };
+
   return (
     <Modal isOpen={modalOpen} handleClose={() => setModalOpen(false)}>
       <div className="fixed top-20 left-1/2 z-10 max-h-[80%] w-4/5 translate-x-[-50%] translate-y-[-10%] overflow-scroll rounded-lg bg-white p-5">
         <h2 className="text-3xl">Add Workout</h2>
         <form onSubmit={handleSubmit} className="flex flex-col">
-          <>
-            <label htmlFor="date">Date:</label>
-            <input
-              className="mb-5 bg-gray-200"
-              type="date"
-              id="date"
-              name="date"
-              value={formData.date}
-              onChange={handleChange}
-            />
-            <label htmlFor="name">Workout Name:</label>
-            <input
-              className="mb-5 bg-gray-200"
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-            <h1 className=" border-b-2 border-black">Exercises:</h1>
-            {formData.exercises.map((exercise, exerciseIndex) => (
-              <div
-                key={exerciseIndex}
-                className="mt-2 mb-5 flex flex-col gap-4"
+          <label htmlFor="date">Date:</label>
+          <input
+            className="mb-5 bg-gray-200"
+            type="date"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
+          <label htmlFor="name">Workout Name:</label>
+          <input
+            className="mb-5 bg-gray-200"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <h1 className=" border-b-2 border-black">Exercises:</h1>
+          {formData.exercises.map((exercise, exerciseIndex) => (
+            <div key={exerciseIndex} className="mt-2 mb-5 flex flex-col gap-4">
+              {/* TODO: This would be a search of a list of exercises */}
+              <input
+                type="text"
+                placeholder="Exercise Name"
+                name="exerciseName"
+                value={exercise.name}
+                onChange={(event) =>
+                  handleExerciseNameChange(exerciseIndex, event)
+                }
+                // TODO: Maybe have main exercise name (bench press, squat, deadlift) in one dropdown
+                // Then another dropdown with modifications susch a incline, low bar, pause, pin, bands, chains, etc
+              />
+              {exercise.sets.map((set, setIndex) => (
+                <div key={setIndex} className="flex justify-between">
+                  <h3>Set {setIndex + 1}:</h3>
+                  <input
+                    type="text"
+                    inputMode="numeric"
+                    placeholder="Reps"
+                    name="reps"
+                    value={set.reps}
+                    className="w-16"
+                    onChange={(event) =>
+                      handleRepsChange(exerciseIndex, setIndex, event)
+                    }
+                  />
+                  <input
+                    type="text"
+                    inputMode="decimal"
+                    placeholder="Weight"
+                    name="weight"
+                    value={set.weight}
+                    className=" w-16"
+                    onChange={(event) =>
+                      handleWeightChange(exerciseIndex, setIndex, event)
+                    }
+                  />
+                </div>
+              ))}
+              <button
+                type="button"
+                onClick={() => handleAddSet(exerciseIndex)}
+                className="w-20 rounded-md bg-green-500 py-1 px-2 text-sm text-white"
               >
-                {/* TODO: This would be a search of a list of exercises */}
-                <input
-                  type="text"
-                  placeholder="Exercise Name"
-                  value={exercise.name}
-                  onChange={(event) =>
-                    handleExerciseNameChange(exerciseIndex, event)
-                  }
-                  // TODO: Maybe have main exercise name (bench press, squat, deadlift) in one dropdown
-                  // Then another dropdown with modifications susch a incline, low bar, pause, pin, bands, chains, etc
-                />
-                {exercise.sets.map((set, setIndex) => (
-                  <div key={setIndex} className="flex justify-between">
-                    <h3>Set {setIndex + 1}:</h3>
-                    <input
-                      type="text"
-                      inputMode="numeric"
-                      placeholder="Reps"
-                      value={set.reps}
-                      className="w-16"
-                      onChange={(event) =>
-                        handleRepsChange(exerciseIndex, setIndex, event)
-                      }
-                    />
-                    <input
-                      type="text"
-                      inputMode="decimal"
-                      placeholder="Weight"
-                      value={set.weight}
-                      className=" w-16"
-                      onChange={(event) =>
-                        handleWeightChange(exerciseIndex, setIndex, event)
-                      }
-                    />
-                  </div>
-                ))}
-                <button
-                  type="button"
-                  onClick={() => handleAddSet(exerciseIndex)}
-                  className="w-20 rounded-md bg-green-500 py-1 px-2 text-sm text-white"
-                >
-                  Add Set
-                </button>
-                <textarea
-                  placeholder="Notes"
-                  value={exercise.notes}
-                  onChange={(event) =>
-                    handleExerciseNotesChange(exerciseIndex, event)
-                  }
-                />
-              </div>
-            ))}
-            <button
-              type="button"
-              className="my-4 rounded-md border-2 border-black"
-              onClick={handleAddExercise}
-            >
-              Add Exercise
-            </button>
-            <button
-              type="submit"
-              className="self-center rounded-md bg-blue-400 p-2 text-white"
-            >
-              Submit
-            </button>
-          </>
+                Add Set
+              </button>
+              <textarea
+                placeholder="Notes"
+                name="notes"
+                value={exercise.notes}
+                onChange={(event) =>
+                  handleExerciseNotesChange(exerciseIndex, event)
+                }
+              />
+            </div>
+          ))}
+          <button
+            type="button"
+            className="my-4 rounded-md border-2 border-black"
+            onClick={handleAddExercise}
+          >
+            Add Exercise
+          </button>
+          <button
+            type="submit"
+            className="self-center rounded-md bg-blue-400 p-2 text-white"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </Modal>
