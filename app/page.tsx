52d78e35f5f0ff27a73d1ddb17d86f3c00be5818
Kevin Tatooles <tatooles@wisc.edu>
2023-04-24 22:11:04 -0500

    Add clerk and sign in and sign up buttons

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,13 @@ export default function Home() {
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <div className="mb-10 text-3xl">Welcome</div>
-      <Link href="/home" className="mb-5 rounded-md bg-gray-600 p-2 text-white">
+      <Link
+        href="/sign-in"
+        className="mb-5 rounded-md bg-gray-600 p-2 text-white"
+      >
         Login
       </Link>
-      <Link href="/home" className="rounded-md bg-gray-600 p-2 text-white">
+      <Link href="/sign-up" className="rounded-md bg-gray-600 p-2 text-white">
         Sign Up
       </Link>
     </div>
