a02018571a9b067535999088bd30e1bb064b2d73
Kevin Tatooles <tatooles@wisc.edu>
2023-04-24 21:23:16 -0500

    Move route to pages directory

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-const handler = NextAuth({
+export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -10,6 +10,6 @@ const handler = NextAuth({
     }),
     // ...add more providers here
   ],
-});
+};
 
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
