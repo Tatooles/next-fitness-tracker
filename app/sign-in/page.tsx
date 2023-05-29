f5289a09eacf6894042ebf9586d31087a2c72a39
Kevin Tatooles <tatooles@wisc.edu>
2023-05-29 16:26:09 -0500

    Revert url change

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,8 +3,8 @@ import { SignIn } from "@clerk/nextjs/app-beta";
 export default function Page() {
   return (
     <div className="flex justify-center">
-      <SignIn signUpUrl="/sign-up" redirectUrl={"www.liftinglog.app/home"} />
-      {/* TODO: I think this redirectUrl is a relative path, needs to be absolute */}
+      <SignIn signUpUrl="/sign-up" redirectUrl={"/home"} />
+      {/* TODO: I think this redirectUrl is a relative path, may need to be absolute */}
     </div>
   );
 }
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs/app-beta";
 export default function Page() {
   return (
     <div className="flex justify-center">
-      <SignUp signInUrl="/sign-in" redirectUrl={"www.liftinglog.app/home"} />
+      <SignUp signInUrl="/sign-in" redirectUrl={"/home"} />
     </div>
   );
 }
