702473e1e46fc09cd0ad60096235412c3a3715c9
Kevin Tatooles <tatooles@wisc.edu>
2023-05-28 20:53:24 -0500

    Try updating redirect url

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,7 +3,8 @@ import { SignIn } from "@clerk/nextjs/app-beta";
 export default function Page() {
   return (
     <div className="flex justify-center">
-      <SignIn signUpUrl="/sign-up" redirectUrl={"/home"} />
+      <SignIn signUpUrl="/sign-up" redirectUrl={"www.liftinglog.app/home"} />
+      {/* TODO: I think this redirectUrl is a relative path, needs to be absolute */}
     </div>
   );
 }
