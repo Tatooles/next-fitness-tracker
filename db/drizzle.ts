2630b53e39f5560f55c222b07f00926576d65fe5
Kevin Tatooles <tatooles@wisc.edu>
2023-06-04 13:55:18 -0500

    Reorganize folders

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -8,4 +8,4 @@ const connection = connect({
   password: process.env["DATABASE_PASSWORD"],
 });
 
-const db = drizzle(connection);
+export const db = drizzle(connection);
---
+++ db/schema.ts
@@ -0,0 +1,2 @@
+// This is where we define the database?
+// At least that's how it's done in fireship's video
