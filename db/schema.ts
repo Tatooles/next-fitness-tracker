6ee1d4bb7689a75594aa97f5bb454125d5890a3a
Kevin Tatooles <tatooles@wisc.edu>
2023-06-10 18:11:06 -0500

    Schema updates and comments

--- db/schema.ts
+++ db/schema.ts
@@ -6,7 +6,7 @@ import {
   datetime,
   text,
 } from "drizzle-orm/mysql-core";
-import { InferModel } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 
 export const workouts = mysqlTable("workouts", {
   id: serial("id").primaryKey(),
@@ -15,22 +15,40 @@ export const workouts = mysqlTable("workouts", {
   date: datetime("date"),
 });
 
-// export type Workout = InferModel<typeof workouts>;
+// TODO: Need something like this for the relational DB
+export const workoutsRelations = relations(workouts, ({ many }) => ({
+  exercises: many(exercises),
+}));
 
-// export const exercises = mysqlTable("exercises", {
-//   id: serial("id").primaryKey(),
-//   name: varchar("name", { length: 256 }),
-//   notes: text("notes"),
-//   // Need FK to workouts as well
-// });
+export const exercises = mysqlTable("exercises", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }),
+  notes: text("notes"),
+  workoutId: int("workout_id").notNull(),
+  // Need FK to workouts as well
+});
 
-// export type Exercise = InferModel<typeof exercises>;
+// TODO: Should be like this
+// export const postsRelations = relations(posts, ({ one, many }) => ({
+//   author: one(users, {
+//     fields: [posts.authorId],
+//     references: [users.id],
+//   }),
+//   comments: many(comments),
+// }));
 
-// export const sets = mysqlTable("sets", {
-//   id: serial("id").primaryKey(),
-//   reps: varchar("reps", { length: 16 }),
-//   weight: varchar("weight", { length: 16 }),
-//   // Need FK to exercises as well
-// });
+export const exercisesRelations = relations(exercises, ({ one }) => ({
+  author: one(workouts, {
+    fields: [exercises.workoutId],
+    references: [workouts.id],
+  }),
+}));
 
-// export type Set = InferModel<typeof sets>;
+// Also one to many for exercises to sets
+
+export const sets = mysqlTable("sets", {
+  id: serial("id").primaryKey(),
+  reps: varchar("reps", { length: 16 }),
+  weight: varchar("weight", { length: 16 }),
+  // Need FK to exercises as well
+});
