de469e0293478eb15904566adf8202813a110cd8
Kevin Tatooles <tatooles@wisc.edu>
2023-06-12 21:30:21 -0500

    Add relations to finish schema

--- db/schema.ts
+++ db/schema.ts
@@ -10,12 +10,11 @@ import { relations } from "drizzle-orm";
 
 export const workouts = mysqlTable("workouts", {
   id: serial("id").primaryKey(),
-  userId: int("userId"), // Double check type, int should be fine
+  userId: int("userId"),
   name: varchar("name", { length: 256 }),
   date: datetime("date"),
 });
 
-// TODO: Need something like this for the relational DB
 export const workoutsRelations = relations(workouts, ({ many }) => ({
   exercises: many(exercises),
 }));
@@ -25,30 +24,26 @@ export const exercises = mysqlTable("exercises", {
   name: varchar("name", { length: 256 }),
   notes: text("notes"),
   workoutId: int("workout_id").notNull(),
-  // Need FK to workouts as well
 });
 
-// TODO: Should be like this
-// export const postsRelations = relations(posts, ({ one, many }) => ({
-//   author: one(users, {
-//     fields: [posts.authorId],
-//     references: [users.id],
-//   }),
-//   comments: many(comments),
-// }));
-
-export const exercisesRelations = relations(exercises, ({ one }) => ({
-  author: one(workouts, {
+export const exercisesRelations = relations(exercises, ({ one, many }) => ({
+  sets: many(sets),
+  workout: one(workouts, {
     fields: [exercises.workoutId],
     references: [workouts.id],
   }),
 }));
 
-// Also one to many for exercises to sets
-
 export const sets = mysqlTable("sets", {
   id: serial("id").primaryKey(),
   reps: varchar("reps", { length: 16 }),
   weight: varchar("weight", { length: 16 }),
-  // Need FK to exercises as well
+  exerciseId: int("exercise_id").notNull(),
 });
+
+export const setsRelations = relations(sets, ({ one }) => ({
+  exercise: one(exercises, {
+    fields: [sets.exerciseId],
+    references: [exercises.id],
+  }),
+}));
