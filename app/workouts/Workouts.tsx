41a2abc7c1f3c52e71a42caa879649b749fdb05d
Kevin Tatooles <tatooles@wisc.edu>
2023-06-20 22:59:27 -0500

    Add template code for delete workout

--- app/workouts/Workouts.tsx
+++ app/workouts/Workouts.tsx
@@ -4,10 +4,9 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-
+import { Button } from "@/components/ui/button";
 import { Workout, Exercise, Set } from "@/lib/types";
-// TODO: This is currently a client component, we will want it to be a server component
-// Need to do this: https://beta.nextjs.org/docs/rendering/server-and-client-components#importing-server-components-into-client-components
+
 export default function Workouts({
   workouts,
   editWorkout,
@@ -19,6 +18,12 @@ export default function Workouts({
     return new Date(date).toLocaleDateString();
   };
 
+  const deleteWorkout = (workout: Workout) => {
+    console.log("Deleting workout", workout.name);
+    // TODO: use db workout object or add id to the current workout object
+    // Should probably try to use the db object
+  };
+
   return (
     <Accordion type="single" collapsible className="mb-5">
       {workouts.map((workout: Workout, index) => (
@@ -26,7 +31,17 @@ export default function Workouts({
           <AccordionTrigger>{workout.name}</AccordionTrigger>
           <AccordionContent>
             {/* <AccordionContent onClick={() => editWorkout(index)}> */}
-            <h3 className="text-md p-2 text-left">{getDate(workout.date)}</h3>
+            {/* <h3 className="text-md p-2 text-left">{getDate(workout.date)}</h3> */}
+            <div className="flex justify-between">
+              <div className="p-2">{getDate(workout.date)}</div>
+              <Button
+                onClick={() => deleteWorkout(workout)}
+                className="py-1 px-2"
+                variant="destructive"
+              >
+                Delete
+              </Button>
+            </div>
             <div className="divide-y-2 px-2">
               {workout.exercises.map((exercise: Exercise, index2) => (
                 <Exercise exercise={exercise} key={index2}></Exercise>
