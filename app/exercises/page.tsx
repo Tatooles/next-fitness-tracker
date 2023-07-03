a0e5f56eaff73aa7189698034765dc049cc680f7
Kevin Tatooles <tatooles@wisc.edu>
2023-07-02 21:06:42 -0500

    Example query using the sql operator

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,18 +1,32 @@
 import { auth } from "@clerk/nextjs";
 import { db } from "@/db/drizzle";
+import { sql } from "drizzle-orm";
 import { Exercise } from "@/lib/types";
+import { exercises, workouts } from "@/db/schema";
 
 async function getExercises() {
   const userId = auth().userId;
-  // if (userId) {
-  //   const data: Exercise[] = await db.query.exercises.findMany();
-  //   return data;
-  // } else {
-  return ["exercise 1", "exercise 2"];
-  // }
+  if (userId) {
+    //   const data: Exercise[] = await db.query.exercises.findMany();
+    //   return data;
+    const data = await db.execute(sql`select ${exercises}.*, ${workouts.date} 
+    from ${exercises} 
+    join ${workouts} on ${exercises.workoutId} = ${workouts.id}
+    where ${workouts.userId} = ${userId}`);
+    return data;
+  } else {
+    return [];
+  }
 }
 
 export default async function ExercisesPage() {
   const exercises = await getExercises();
-  return <div>{exercises}</div>;
+  console.log(exercises);
+  return (
+    <div>
+      {/* {exercises.map((exercise) => (
+        <div>{exercise.name}</div>
+      ))} */}
+    </div>
+  );
 }
