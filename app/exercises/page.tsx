03abeca30eca7d7c7bc0511e35f55b28bd558e1d
Kevin Tatooles <tatooles@wisc.edu>
2023-07-02 23:00:26 -0500

    Work on placing sets into accordion

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,18 +1,34 @@
 import { auth } from "@clerk/nextjs";
 import { db } from "@/db/drizzle";
 import { sql } from "drizzle-orm";
+import { eq } from "drizzle-orm";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Exercise } from "@/lib/types";
-import { exercises, workouts } from "@/db/schema";
+import { exercises, sets, workouts } from "@/db/schema";
 
 async function getExercises() {
   const userId = auth().userId;
   if (userId) {
     //   const data: Exercise[] = await db.query.exercises.findMany();
     //   return data;
-    const data = await db.execute(sql`select ${exercises}.*, ${workouts.date} 
-    from ${exercises} 
-    join ${workouts} on ${exercises.workoutId} = ${workouts.id}
-    where ${workouts.userId} = ${userId}`);
+    const data = await db.query.workouts.findMany({
+      where: (workouts, { eq }) => eq(workouts.userId, userId),
+      columns: {
+        date: true,
+      },
+      with: {
+        exercises: {
+          with: {
+            sets: true,
+          },
+        },
+      },
+    });
     return data;
   } else {
     return [];
@@ -20,13 +36,42 @@ async function getExercises() {
 }
 
 export default async function ExercisesPage() {
-  const exercises = await getExercises();
-  console.log(exercises);
+  const workouts = await getExercises();
   return (
-    <div>
-      {/* {exercises.map((exercise) => (
-        <div>{exercise.name}</div>
-      ))} */}
+    <div className="p-5 text-center">
+      <h1 className="mb-5 text-3xl">Exercises</h1>
+      <Accordion type="single" collapsible className="mb-5">
+        {workouts.map((workout, index) => (
+          <div key={index}>
+            {workout.exercises.map((exercise) => (
+              <ExerciseItem
+                date={workout.date}
+                exercise={exercise}
+              ></ExerciseItem>
+            ))}
+          </div>
+        ))}
+      </Accordion>
     </div>
+  );
+}
+
+function ExerciseItem({ date, exercise }: { date: Date; exercise: Exercise }) {
+  console.log(date, exercise);
+  return (
+    <AccordionItem key={exercise.id} value={`exercise-${exercise.id}`}>
+      <AccordionTrigger>{exercise.name}</AccordionTrigger>
+      <AccordionContent>
+        <div className="p-2 text-left">
+          <h3 className="text-lg font-bold">{date.toLocaleDateString()}</h3>
+          {exercise.sets.map((set) => {
+            <div key={set.id} className="flex">
+              <div>{set.reps}</div>
+              <div>{set.weight}</div>
+            </div>;
+          })}
+        </div>
+      </AccordionContent>
+    </AccordionItem>
   );
 }
