fcfcc73e743adac4f0c3d50626576e8e94182474
Kevin Tatooles <tatooles@wisc.edu>
2023-08-03 23:17:48 -0500

    Move side nav links into sheet

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
 import {
   Sheet,
@@ -15,27 +16,35 @@ export default function Navbar() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <>
-      <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent side="left">
-          <SheetHeader>
-            <SheetTitle>Are you sure absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
       <header>
         <nav className="flex items-center justify-between border-b-2 text-lg text-black">
-          <div onClick={() => setSidebarOpen(true)} className="p-5">
-            <div className="flex h-[20px] w-[20px] flex-col justify-between overflow-hidden">
-              <div className="h-[2px] w-7 bg-slate-700"></div>
-              <div className="h-[2px] w-7 bg-slate-700"></div>
-              <div className="h-[2px] w-7 bg-slate-700 "></div>
-            </div>
-          </div>
+          <Sheet>
+            <SheetTrigger>
+              <div className="p-5">
+                <div className="flex h-[20px] w-[20px] flex-col justify-between overflow-hidden">
+                  <div className="h-[2px] w-7 bg-slate-700"></div>
+                  <div className="h-[2px] w-7 bg-slate-700"></div>
+                  <div className="h-[2px] w-7 bg-slate-700 "></div>
+                </div>
+              </div>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <ul className="flex flex-col space-y-1">
+                <Link onClick={() => setIsOpen(false)} href="/">
+                  Home
+                </Link>
+                <Link onClick={() => setIsOpen(false)} href="/workouts">
+                  Workouts
+                </Link>
+                <Link onClick={() => setIsOpen(false)} href="/exercises">
+                  Exercises
+                </Link>
+                <Link onClick={() => setIsOpen(false)} href="/settings">
+                  Settings
+                </Link>
+              </ul>
+            </SheetContent>
+          </Sheet>
           <div className="p-5">
             <UserButton></UserButton>
           </div>
