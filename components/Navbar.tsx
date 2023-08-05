db2eede8410c3e612ea23b7293170a3f93796ad1
Kevin Tatooles <tatooles@wisc.edu>
2023-08-05 17:13:02 -0500

    Code cleanup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,14 +5,7 @@ import { usePathname } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 interface SidebarItem {
   title: string;
@@ -29,45 +22,42 @@ export default function Navbar({
   console.log(pathName);
 
   return (
-    <>
-      <header>
-        <nav className="flex items-center justify-between border-b-2 text-lg text-black">
-          <Sheet open={sidebarOpen} onOpenChange={setSidebarOpen}>
-            {/* TODO: Disable auto select of x button (prob bad for accessibility) */}
-            <SheetTrigger>
-              <div className="p-5">
-                <div className="flex h-[20px] w-[20px] flex-col justify-between overflow-hidden">
-                  <div className="h-[2px] w-7 bg-slate-700"></div>
-                  <div className="h-[2px] w-7 bg-slate-700"></div>
-                  <div className="h-[2px] w-7 bg-slate-700 "></div>
-                </div>
-              </div>
-            </SheetTrigger>
-            <SheetContent side="left" className="w-[250px]">
-              <nav className="flex flex-col space-y-2">
-                {sidebarItems.map((item) => (
-                  <Link
-                    onClick={() => setSidebarOpen(false)}
-                    key={item.href}
-                    href={item.href}
-                    className={cn(
-                      pathName === item.href
-                        ? "bg-muted hover:bg-muted"
-                        : "hover:underline",
-                      "rounded-md p-2 text-2xl"
-                    )}
-                  >
-                    {item.title}
-                  </Link>
-                ))}
-              </nav>
-            </SheetContent>
-          </Sheet>
+    <header className="flex items-center justify-between border-b-2 text-lg text-black">
+      <Sheet open={sidebarOpen} onOpenChange={setSidebarOpen}>
+        <SheetTrigger>
           <div className="p-5">
-            <UserButton></UserButton>
+            <div className="flex h-[20px] w-[20px] flex-col justify-between overflow-hidden">
+              <div className="h-[2px] w-7 bg-slate-700"></div>
+              <div className="h-[2px] w-7 bg-slate-700"></div>
+              <div className="h-[2px] w-7 bg-slate-700 "></div>
+            </div>
           </div>
-        </nav>
-      </header>
-    </>
+        </SheetTrigger>
+        <SheetContent
+          onOpenAutoFocus={(e) => e.preventDefault()}
+          side="left"
+          className="w-[200px]"
+        >
+          <nav className="flex flex-col space-y-2">
+            {sidebarItems.map((item) => (
+              <Link
+                onClick={() => setSidebarOpen(false)}
+                key={item.href}
+                href={item.href}
+                className={cn(
+                  pathName === item.href ? "bg-muted" : "hover:underline",
+                  "w-5/6 rounded-md p-2 text-xl"
+                )}
+              >
+                {item.title}
+              </Link>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div className="p-5">
+        <UserButton></UserButton>
+      </div>
+    </header>
   );
 }
