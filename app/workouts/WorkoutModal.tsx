564350f17392738cacbe9c6bb2c010fb425335df
Kevin Tatooles <tatooles@wisc.edu>
2023-08-15 23:20:51 -0500

    Create basic dynamic inputs

--- app/workouts/WorkoutModal.tsx
+++ app/workouts/WorkoutModal.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { format } from "date-fns";
 import * as z from "zod";
@@ -35,13 +35,18 @@ import {
 import { Workout } from "@/lib/types";
 
 const formSchema = z.object({
-  date: z.date(),
-  name: z
-    .string()
-    .min(1, {
-      message: "Wokout name must be at least 1 character",
+  // date: z.date(),
+  // name: z
+  //   .string()
+  //   .min(1, {
+  //     message: "Wokout name must be at least 1 character",
+  //   })
+  //   .max(50),
+  exercises: z
+    .object({
+      name: z.string(),
     })
-    .max(50),
+    .array(),
   // exercises: z
   //   .object({
   //     name: z.string(),
@@ -77,22 +82,22 @@ export default function WorkoutModal({
    * workout, the workout in the form is compared to the workout that was passed in and the save
    * call is only made if the value was changed
    */
-  const handleSubmit = async () => {
-    // TODO: Replace this function with a server action
-    setModalOpen(false);
-    setShowSpinner(true);
+  // const handleSubmit = async () => {
+  //   // TODO: Replace this function with a server action
+  //   setModalOpen(false);
+  //   setShowSpinner(true);
 
-    // if (!editWorkoutValue || editWorkoutValue.id === -2) {
-    //   // If adding or duplicating, just create new workout
-    //   await addToDB();
-    //   router.refresh();
-    // } else if (JSON.stringify(editWorkoutValue) !== JSON.stringify(formData)) {
-    //   // If updating, call delete to delete the existing workout, then addToDB to add udpated one
-    //   await Promise.all([deleteWorkout(editWorkoutValue.id), addToDB()]);
-    //   router.refresh();
-    // }
-    setShowSpinner(false);
-  };
+  //   // if (!editWorkoutValue || editWorkoutValue.id === -2) {
+  //   //   // If adding or duplicating, just create new workout
+  //   //   await addToDB();
+  //   //   router.refresh();
+  //   // } else if (JSON.stringify(editWorkoutValue) !== JSON.stringify(formData)) {
+  //   //   // If updating, call delete to delete the existing workout, then addToDB to add udpated one
+  //   //   await Promise.all([deleteWorkout(editWorkoutValue.id), addToDB()]);
+  //   //   router.refresh();
+  //   // }
+  //   setShowSpinner(false);
+  // };
 
   const addToDB = async () => {
     await fetch("/api/workouts", {
@@ -130,12 +135,18 @@ export default function WorkoutModal({
     console.log(values);
   };
 
-  const form = useForm<workoutFormSchema>({
+  const { handleSubmit, control, register } = useForm<workoutFormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      date: new Date(),
-      name: "",
+      // date: new Date(),
+      // name: "",
+      exercises: [{ name: "" }],
     },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    name: "exercises",
+    control,
   });
 
   return (
@@ -150,67 +161,91 @@ export default function WorkoutModal({
             {editWorkoutValue ? "Edit Workout" : "Create Workout"}
           </DialogTitle>
         </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="date"
-              render={({ field }) => (
-                <FormItem className="flex flex-col">
-                  <FormLabel>Date</FormLabel>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <FormControl>
-                        <Button
-                          variant={"outline"}
-                          className={cn(
-                            "w-[240px] pl-3 text-left font-normal",
-                            !field.value && "text-muted-foreground"
-                          )}
-                        >
-                          {field.value ? (
-                            format(field.value, "PPP")
-                          ) : (
-                            <span>Pick a date</span>
-                          )}
-                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                        </Button>
-                      </FormControl>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar
-                        mode="single"
-                        selected={field.value}
-                        onSelect={field.onChange}
-                        disabled={(date) =>
-                          date > new Date() || date < new Date("1900-01-01")
-                        }
-                        initialFocus
-                      />
-                    </PopoverContent>
-                  </Popover>
-                </FormItem>
-              )}
-            ></FormField>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Workout Name</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Enter the name of this workout.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            ></FormField>
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
+        {/* <Form> */}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* <FormField
+            control={control}
+            name="date"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Date</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            control={control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Workout Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                  Enter the name of this workout.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField> */}
+
+          <div>
+            <Label>Exercises</Label>
+            <div>
+              {fields.map((field, index) => (
+                <div className="form-control" key={field.id}>
+                  <Input
+                    {...register(`exercises.${index}.name` as const)}
+                    placeholder="Exercise name"
+                  />
+                  <Button
+                    type="button"
+                    variant="destructive"
+                    onClick={() => remove(index)}
+                  >
+                    Remove Exercise
+                  </Button>
+                </div>
+              ))}
+              <Button type="button" onClick={() => append({ name: "" })}>
+                Add Exercise
+              </Button>
+            </div>
+          </div>
+          <Button type="submit">Submit</Button>
+        </form>
+        {/* </Form> */}
       </DialogContent>
     </Dialog>
   );
