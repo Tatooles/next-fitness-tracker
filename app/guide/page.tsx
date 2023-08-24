9013e8d2ab7c8573d3a1689a3e7f65c9ce93e343
Kevin Tatooles <tatooles@wisc.edu>
2023-08-23 22:56:16 -0500

    Continue to flesh out guide page

---
+++ app/guide/page.tsx
@@ -0,0 +1,42 @@
+export default function HowTo() {
+  return (
+    <div className="p-5 text-center">
+      <h1 className="mb-5 text-3xl">Usage Guide</h1>
+      <div className="text-left">
+        <p>
+          Begin by going to the Workouts page and creating a workout by clicking
+          "Add a Workout"
+        </p>
+        <img
+          src=""
+          alt="Screenshot of add workout button"
+          className="border-2 border-black"
+        />
+        <p>
+          You can enter the name and date of your workout as well as specific
+          exercise names.
+        </p>
+        <img
+          src=""
+          alt="Screenshot of top of workout section"
+          className="border-2 border-black"
+        />
+        <p>For each exercise you can enter sets with reps and weight</p>
+        <p>You can also add notes for each exercise</p>
+        <img
+          src=""
+          alt="Screenshot of exercise section"
+          className="border-2 border-black"
+        />
+        <p>
+          Once you have finished entering your workout, press submit to save the
+          workout to our servers
+        </p>
+        <p>
+          See the history of all the workouts you have completed on the workouts
+          page
+        </p>
+      </div>
+    </div>
+  );
+}
