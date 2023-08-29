c5c2ecde4ed58f737f3048e461294c8ada9f2f57
Kevin Tatooles <tatooles@wisc.edu>
2023-08-28 22:14:26 -0500

    Make guide page responsive and add alt text

--- app/guide/page.tsx
+++ app/guide/page.tsx
@@ -1,32 +1,35 @@
 import Image from "next/image";
+import addWorkoutImage from "public/add-workout-image.png";
+import duplicateWorkoutImage from "public/duplicate-workout-image.png";
+import exerciseImage from "public/exercise-image.png";
+import exercisesImage from "public/exercises-image.png";
+import submitImage from "public/submit-image.png";
+import workoutNameDateImage from "public/workout-date-name-image.png";
+import workoutHistoryImage from "public/workout-history-image.png";
 
 export default function HowTo() {
   return (
     <div className="p-5 text-center">
       <h1 className="mb-5 text-3xl">Usage Guide</h1>
-      <article className="text-left">
+      <article className="mx-auto text-left sm:w-3/4 lg:w-3/5">
         <h2 className="mb-2 text-xl underline">Workouts page</h2>
         <p>
           Begin by going to the Workouts page and creating a workout by clicking
           &quot;Add a Workout&quot;
         </p>
         <Image
-          src="/add-workout-image.png"
-          alt="Screenshot of add workout button"
-          width={300}
-          height={300}
-          className="my-4 border-2 border-black"
+          src={addWorkoutImage}
+          alt="Screenshot of the add workout button"
+          className="my-4 h-auto w-full border-2 border-black sm:w-2/3"
         />
         <p>
           Enter the name and date of your workout. The date will default to
           today&apos;s date.
         </p>
         <Image
-          src="/workout-date-name-image.png"
-          alt="Screenshot of top of workout section"
-          width={300}
-          height={300}
-          className="my-4 border-2 border-black"
+          src={workoutNameDateImage}
+          alt="Screenshot of the top of the create workout section"
+          className="my-4 h-auto w-full border-2 border-black sm:w-2/3"
         />
         <p>
           Now enter the exercises you performed during this workout. For each
@@ -34,11 +37,9 @@ export default function HowTo() {
           as well as notes aobut the exercise.
         </p>
         <Image
-          src="/exercise-image.png"
+          src={exerciseImage}
           alt="Screenshot of exercise section"
-          width={300}
-          height={300}
-          className="my-4 border-2 border-black"
+          className="my-4 h-auto w-full border-2 border-black sm:w-1/2"
         />
         <p>
           You can press &quot;Add set&quot; to create a new blank set, or
@@ -51,11 +52,9 @@ export default function HowTo() {
           your workout to our servers.
         </p>
         <Image
-          src="/submit-image.png"
-          alt=""
-          width={300}
-          height={300}
-          className="my-4 border-2 border-black"
+          src={submitImage}
+          alt="Screeshot of submit button"
+          className="my-4 h-auto w-full border-2 border-black sm:w-2/3"
         />
         <p>
           After entering your workout, it will appear under the &quot;Add
@@ -63,11 +62,9 @@ export default function HowTo() {
           have completed on the workouts page. Tap to expand a specific workout.
         </p>
         <Image
-          src="/workout-history-image.png"
-          alt=""
-          width={300}
-          height={300}
-          className="my-4 border-2 border-black"
+          src={workoutHistoryImage}
+          alt="Screenshot of recent workouts on workouts page"
+          className="my-4 h-auto w-full border-2 border-black sm:w-1/2"
         />
         <p>
           If you have a long workout and would like to enter information as you
@@ -81,23 +78,19 @@ export default function HowTo() {
           but with the weight and notes fields emptied.
         </p>
         <Image
-          src="/duplicate-workout-image.png"
-          alt=""
-          width={300}
-          height={300}
-          className="my-4 border-2 border-black"
+          src={duplicateWorkoutImage}
+          alt="Screenshot of duplicated workout"
+          className="my-4 h-auto w-full border-2 border-black sm:w-1/2"
         />
         <h2 className="mb-2 text-xl underline">Exercises page</h2>
         <p>
-          One the Exercises page you can see a list of all the exercises you
-          have logged. Use the search bar to filter your exercises by name.
+          On the Exercises page you can see a list of all the exercises you have
+          logged. Use the search bar to filter your exercises by name.
         </p>
         <Image
-          src="/exercises-image.png"
-          alt=""
-          width={300}
-          height={300}
-          className="my-4 border-2 border-black"
+          src={exercisesImage}
+          alt="Screenshot of exercise list filtered by name"
+          className="my-4 h-auto w-full border-2 border-black sm:w-1/2"
         />
       </article>
     </div>
