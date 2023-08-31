54ab79e97c547ae2bda6392df2df8498258cac96
Kevin Tatooles <tatooles@wisc.edu>
2023-08-30 21:16:30 -0500

    Implement copy suggestions

--- app/guide/page.tsx
+++ app/guide/page.tsx
@@ -12,10 +12,10 @@ export default function HowTo() {
     <div className="p-5 text-center">
       <h1 className="mb-5 text-3xl">Usage Guide</h1>
       <article className="mx-auto text-left sm:w-3/4 lg:w-3/5">
-        <h2 className="mb-2 text-xl underline">Workouts page</h2>
+        <h2 className="mb-2 text-xl underline">Workouts Page</h2>
         <p>
-          Begin by going to the Workouts page and creating a workout by clicking
-          &quot;Add a Workout&quot;
+          Navigate to the Workouts page and create a workout by clicking
+          &quot;Add a Workout.&quot;
         </p>
         <Image
           src={addWorkoutImage}
@@ -23,7 +23,7 @@ export default function HowTo() {
           className="my-4 h-auto w-full border-2 border-black sm:w-2/3"
         />
         <p>
-          Enter the name and date of your workout. The date will default to
+          Enter the name and date of the workout. The date will default to
           today&apos;s date.
         </p>
         <Image
@@ -32,9 +32,9 @@ export default function HowTo() {
           className="my-4 h-auto w-full border-2 border-black sm:w-2/3"
         />
         <p>
-          Now enter the exercises you performed during this workout. For each
-          exercise you can enter the reps and weight for each set you performed,
-          as well as notes aobut the exercise.
+          Enter the exercises performed during this workout. For each exercise
+          enter the reps and weight for each set performed, as well as notes
+          about the exercise.
         </p>
         <Image
           src={exerciseImage}
@@ -42,14 +42,13 @@ export default function HowTo() {
           className="my-4 h-auto w-full border-2 border-black sm:w-1/2"
         />
         <p>
-          You can press &quot;Add set&quot; to create a new blank set, or
-          &quot;Clone set&quot; to make a copy of the reps and sets of the last
-          set in the list. Use the &quot;-&quot; button to remove a set or
-          exercise.
+          Press &quot;Add set&quot; to create a new blank set, or &quot;Clone
+          set&quot; to make a copy of the reps and weight of the last set in the
+          list. Use the &quot;-&quot; button to remove a set or exercise.
         </p>
         <p className="mt-4">
-          Once you have finished entering your workout, press submit to save
-          your workout to our servers.
+          Once a workout has been entered, press submit to save the workout to
+          the Lifting Log.
         </p>
         <Image
           src={submitImage}
@@ -57,9 +56,9 @@ export default function HowTo() {
           className="my-4 h-auto w-full border-2 border-black sm:w-2/3"
         />
         <p>
-          After entering your workout, it will appear under the &quot;Add
-          workout&quot; button. You can see the history of all the workouts you
-          have completed on the workouts page. Tap to expand a specific workout.
+          After entering a workout, it will be visible under the &quot;Add a
+          workout&quot; button. The history of all the workouts completed is
+          visible on the workouts page. Tap to expand a specific workout.
         </p>
         <Image
           src={workoutHistoryImage}
@@ -67,25 +66,24 @@ export default function HowTo() {
           className="my-4 h-auto w-full border-2 border-black sm:w-1/2"
         />
         <p>
-          If you have a long workout and would like to enter information as you
-          go, or if you would like to update a previous workout, use the
-          &quot;Edit&quot; button.
+          For long workouts where entering information as you go is a benefit,
+          or to update a previous workout, use the &quot;Edit&quot; button.
         </p>
         <p className="mt-4">
-          If you are performing a workout that has similar exercises to a
-          previous workout, use the &quot;Duplicate&quot; button. This will
-          create a new workout with the same exercies as the orignal workout,
-          but with the weight and notes fields emptied.
+          When performing a workout that has similar exercises to a previous
+          workout, use the &quot;Duplicate&quot; button. This creates a new
+          workout with the same exercises as the original workout with the
+          weight and notes fields emptied.
         </p>
         <Image
           src={duplicateWorkoutImage}
           alt="Screenshot of duplicated workout"
           className="my-4 h-auto w-full border-2 border-black sm:w-1/2"
         />
-        <h2 className="mb-2 text-xl underline">Exercises page</h2>
+        <h2 className="mb-2 text-xl underline">Exercises Page</h2>
         <p>
-          On the Exercises page you can see a list of all the exercises you have
-          logged. Use the search bar to filter your exercises by name.
+          On the Exercises page view a list of all the exercises logged. Use the
+          search bar to filter exercises by name.
         </p>
         <Image
           src={exercisesImage}
