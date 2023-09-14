ad1254baff3bd169af4daf8f632c040c65717962
Kevin Tatooles <tatooles@wisc.edu>
2023-09-13 22:07:09 -0500

    Got it working with form hook

--- app/workouts/create/page.tsx
+++ app/workouts/create/page.tsx
@@ -7,23 +7,17 @@ import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import FormSets from "@/components/FormSets";
-import { workoutFormSchema, TWorkoutFormSchema } from "@/lib/types";
-import { E } from "drizzle-orm/select.types.d-e43b2599";
 
 export default async function CreateWorkoutPage() {
-  // const router = useRouter();
-
   /**
    * This function handles the logic of saving the form the user has filled out. This save is for
    * creating new and editing existing as well as duplicate workouts.
    */
-  const onSubmit = async (values: TWorkoutFormSchema) => {
+  const onSubmit = (values: any) => {
     console.log(values);
-    // await addToDB(values);
-    // router.refresh();
   };
 
-  const addToDB = async (form: TWorkoutFormSchema) => {
+  const addToDB = async (form: any) => {
     await fetch("/api/workouts", {
       method: "POST",
       body: JSON.stringify(form),
@@ -55,38 +49,16 @@ export default async function CreateWorkoutPage() {
       });
   };
 
-  const {
-    handleSubmit,
-    control,
-    register,
-    formState: { errors },
-    getValues,
-  } = useForm<TWorkoutFormSchema>({
-    resolver: zodResolver(workoutFormSchema),
-    defaultValues: {
-      date: new Date().toISOString(),
-      name: "",
-      exercises: [{ name: "", notes: "", sets: [{ reps: "", weight: "" }] }],
-    },
-  });
-
-  const { fields, append, remove } = useFieldArray({
-    name: "exercises",
-    control,
-  });
+  const { handleSubmit, register } = useForm();
 
   return (
     <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        console.log(e);
-      }} // Just having handlesubmit at all freezes the whole browser :)
+      onSubmit={handleSubmit(onSubmit)} // Just having handlesubmit at all freezes the whole browser :)
       className="flex flex-col"
     >
       <Label htmlFor="date">Date:</Label>
       <Input
         type="date"
-        id="date"
         className="mt-2 mb-4 text-[16px]"
         {...register("date")}
       ></Input>
@@ -94,71 +66,9 @@ export default async function CreateWorkoutPage() {
       <div className="mt-2 mb-4">
         <Input
           type="text"
-          id="name"
           className="text-[16px]"
           {...register("name")}
         ></Input>
-        {/* {errors.name && (
-          <p className="mt-2 text-sm font-medium text-destructive">{`${errors.name.message}`}</p>
-        )} */}
-        <Label htmlFor="name">Workout Name:</Label>
-        <div className="mt-2 mb-4">
-          <Input
-            type="text"
-            id="name"
-            className="text-[16px]"
-            {...register("name")}
-          ></Input>
-          {errors.name && (
-            <p className="mt-2 text-sm font-medium text-destructive">{`${errors.name.message}`}</p>
-          )}
-        </div>
-        <div>
-          <Label>Exercises</Label>
-          {fields.map((field, index) => (
-            <div
-              className="form-control flex flex-col gap-4 border-b-2 border-slate-700 px-2 py-4"
-              key={field.id}
-            >
-              <div className="flex items-center">
-                <Input
-                  {...register(`exercises.${index}.name` as const)}
-                  placeholder="Exercise name"
-                  className="text-[16px]"
-                />
-                {/* <div
-                  onClick={() => remove(index)}
-                  className="ml-5 h-6 w-7 cursor-pointer rounded-full bg-red-600 text-center text-white"
-                >
-                  <div className="-translate-y-[1px]">-</div>
-                </div> */}
-              </div>
-              <FormSets
-                exerciseIndex={index}
-                {...{ control, register, getValues }}
-              />
-              <Textarea
-                {...register(`exercises.${index}.notes` as const)}
-                placeholder="Notes"
-                className="text-[16px]"
-              ></Textarea>
-            </div>
-          ))}
-          {/* <Button
-            variant="secondary"
-            className="mt-4 w-full"
-            type="button"
-            onClick={() =>
-              append({
-                name: "",
-                notes: "",
-                sets: [{ reps: "", weight: "" }],
-              })
-            }
-          >
-            Add Exercise
-          </Button> */}
-        </div>
       </div>
       <Button type="submit" className="mt-4 self-center">
         Submit
