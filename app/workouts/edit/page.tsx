464448c7b0f9f4152dce06914e0a680afa38b80f
Kevin Tatooles <tatooles@wisc.edu>
2023-09-19 22:50:15 -0500

    Make a basic workout not found page

--- app/workouts/edit/page.tsx
+++ app/workouts/edit/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { auth } from "@clerk/nextjs";
 import { db } from "@/db/drizzle";
 import { Workout, Exercise } from "@/lib/types";
@@ -26,23 +27,39 @@ export default async function EditWorkoutPage({
   searchParams: { id: number };
 }) {
   const workout = await getWorkout(searchParams.id);
-  // TODO: Show a not found error page if the workout is not found
-  return (
-    <div className="mx-auto p-4 sm:max-w-md">
-      <span>
-        {workout?.date.toLocaleDateString()} - {workout?.name}
-      </span>
-      <div className="flex justify-start pt-2"></div>
-      <div className="text-center">
-        <div className="divide-y-2 px-2">
-          {workout?.exercises.map((exercise: Exercise) => (
-            <div className="p-2" key={exercise.id}>
-              <h3 className="text-lg font-bold">{exercise.name}</h3>
-              <ExerciseItem exercise={exercise}></ExerciseItem>
-            </div>
-          ))}
+
+  if (!workout) {
+    return (
+      <div className="mx-auto flex flex-col p-4 text-center sm:max-w-md">
+        <p className="text-left">
+          Workout not found, please return to the Workouts page
+        </p>
+        <Link
+          className="mx-auto mt-2 rounded-md bg-slate-500 p-2 text-white hover:bg-slate-500/70"
+          href="/workouts"
+        >
+          Workouts
+        </Link>
+      </div>
+    );
+  } else {
+    return (
+      <div className="mx-auto p-4 sm:max-w-md">
+        <span>
+          {workout?.date.toLocaleDateString()} - {workout?.name}
+        </span>
+        <div className="flex justify-start pt-2"></div>
+        <div className="text-center">
+          <div className="divide-y-2 px-2">
+            {workout?.exercises.map((exercise: Exercise) => (
+              <div className="p-2" key={exercise.id}>
+                <h3 className="text-lg font-bold">{exercise.name}</h3>
+                <ExerciseItem exercise={exercise}></ExerciseItem>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
