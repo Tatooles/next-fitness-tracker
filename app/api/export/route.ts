3db7bad1103eabcb341e58f13b884e4ee7c29964
Kevin Tatooles <tatooles@wisc.edu>
2023-10-05 21:08:22 -0500

    Template out some code

---
+++ app/api/export/route.ts
@@ -0,0 +1,7 @@
+// Not sure if we want a get or post route
+// Probably should be a get, we'll fetch all the data on the server
+
+export async function GET(request: Request) {
+  // Want to have the userid and start and end dates as params
+  return new Response("Exporting data");
+}
