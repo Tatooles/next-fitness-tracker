bf982a51cb7c9381842a3e21d0e8e071e348bc8f
Kevin Tatooles <tatooles@wisc.edu>
2023-10-10 21:32:18 -0500

    Add export button

--- app/export/page.tsx
+++ app/export/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default async function ExportPage() {
   return (
-    <div>
-      <div>This is the export page</div>
+    <div className="p-5 text-center">
+      <h1 className="mb-5 text-3xl">Export Workout Data</h1>
+      {/* TODO: Start with just all data, then add date bounds */}
+      <Button className="bg-[#1d6f42]">Export to Excel</Button>
     </div>
   );
 }
