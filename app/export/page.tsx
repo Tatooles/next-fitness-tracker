cf7d04333ad693f3dd4f54f490c99c181045c0e3
Kevin Tatooles <tatooles@wisc.edu>
2023-10-12 22:25:50 -0500

    Add template API call

--- app/export/page.tsx
+++ app/export/page.tsx
@@ -1,11 +1,38 @@
+"use client";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import Spinner from "@/components/Spinner";
 
-export default async function ExportPage() {
+export default function ExportPage() {
+  const [showSpinner, setShowSpinner] = useState(false);
+
+  const getFile = async () => {
+    setShowSpinner(true);
+    console.log("getting file");
+    // await fetch(`/api/workouts/${workout}`, {
+    //   method: "DELETE",
+    // })
+    //   .then((response) => {
+    //     if (response.ok) {
+    //       console.log("response returned");
+    //     } else {
+    //       console.error("Failed to delete exercise.");
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.error("An error occurred while deleting exercise:", error);
+    //   });
+    setShowSpinner(false);
+  };
+
   return (
     <div className="p-5 text-center">
       <h1 className="mb-5 text-3xl">Export Workout Data</h1>
       {/* TODO: Start with just all data, then add date bounds */}
-      <Button className="bg-[#1d6f42]">Export to Excel</Button>
+      <Button onClick={getFile} className="bg-[#1d6f42]">
+        Export to Excel
+      </Button>
+      <Spinner show={showSpinner}></Spinner>
     </div>
   );
 }
