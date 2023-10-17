c0b6e263ffcb1a182d79248df38475ca6942ef6f
Kevin Tatooles <tatooles@wisc.edu>
2023-10-16 21:24:10 -0500

    Add logic to fetch data on backend

--- app/api/export/route.ts
+++ app/api/export/route.ts
@@ -1,8 +1,29 @@
 import * as xlsx from "xlsx";
+import { auth } from "@clerk/nextjs";
+import { db } from "@/db/drizzle";
+import { Workout } from "@/lib/types";
 // Not sure if we want a get or post route
 // Probably should be a get, we'll fetch all the data on the server
 
 export async function GET(request: Request) {
-  // TODO: Can just get id in here, don't event need to pass it from client
+  const userId = auth().userId;
+  console.log(userId);
+  if (userId) {
+    const data: Workout[] = await db.query.workouts.findMany({
+      where: (workouts, { eq }) => eq(workouts.userId, userId),
+      with: {
+        exercises: {
+          with: {
+            sets: {
+              orderBy: (sets, { asc }) => [asc(sets.id)],
+            },
+          },
+        },
+      },
+    });
+    console.log(data);
+  } else {
+    console.log("no user found :(");
+  }
   return new Response("Exporting data");
 }
