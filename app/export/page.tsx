f0b880ddb2e92e07109bc620c99ea8d26eab62c3
Kevin Tatooles <tatooles@wisc.edu>
2023-10-23 22:20:38 -0500

    Fix front end logic

--- app/export/page.tsx
+++ app/export/page.tsx
@@ -1,37 +1,13 @@
-"use client";
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import Spinner from "@/components/Spinner";
+import Link from "next/link";
 
-export default function ExportPage() {
-  const [showSpinner, setShowSpinner] = useState(false);
-
-  const getFile = async () => {
-    setShowSpinner(true);
-    console.log("getting file");
-    try {
-      const response = await fetch(`/api/export`);
-      if (response.ok) {
-        console.log("response returned");
-        const blob = await response.blob();
-        console.log(blob);
-      } else {
-        console.error("Failed to fetch workout data.");
-      }
-    } catch (error) {
-      console.error("An error occurred while fetching workout data:", error);
-    }
-    setShowSpinner(false);
-  };
-
+export default async function ExportPage() {
   return (
     <div className="p-5 text-center">
       <h1 className="mb-5 text-3xl">Export Workout Data</h1>
       {/* TODO: Start with just all data, then add date bounds */}
-      <Button onClick={getFile} className="bg-[#1d6f42]">
+      <Link href="api/export" className="bg-[#1d6f42]">
         Export to Excel
-      </Button>
-      <Spinner show={showSpinner}></Spinner>
+      </Link>
     </div>
   );
 }
