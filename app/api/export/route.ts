c5fd3346e8407c34b617b6eef49f27c26b3157b8
Kevin Tatooles <tatooles@wisc.edu>
2023-11-15 22:15:12 -0600

    Clean up error case

--- app/api/export/route.ts
+++ app/api/export/route.ts
@@ -5,60 +5,56 @@ import { Workout } from "@/lib/types";
 
 export async function GET(request: Request) {
   const userId = auth().userId;
-  console.log(userId);
-  if (userId) {
-    const data: Workout[] = await db.query.workouts.findMany({
-      where: (workouts, { eq }) => eq(workouts.userId, userId),
-      with: {
-        exercises: {
-          with: {
-            sets: {
-              orderBy: (sets, { asc }) => [asc(sets.id)],
-            },
+  if (!userId) {
+    console.log("no user found :(");
+    return Response.error();
+  }
+
+  const data: Workout[] = await db.query.workouts.findMany({
+    where: (workouts, { eq }) => eq(workouts.userId, userId),
+    with: {
+      exercises: {
+        with: {
+          sets: {
+            orderBy: (sets, { asc }) => [asc(sets.id)],
           },
         },
       },
-    });
-    let input = [] as string[][];
-    for (const workout of data) {
-      input.push([workout.name]);
-      for (const exercise of workout.exercises) {
-        input.push([exercise.name]);
-        for (const set of exercise.sets) {
-          input.push([set.reps, set.weight]);
-        }
-        input.push([exercise.notes]);
-        input.push([]);
+    },
+  });
+  let input = [] as string[][];
+  for (const workout of data) {
+    input.push([workout.name]);
+    for (const exercise of workout.exercises) {
+      input.push([exercise.name]);
+      for (const set of exercise.sets) {
+        input.push([set.reps, set.weight]);
       }
-      input.push([]);
+      input.push([exercise.notes]);
       input.push([]);
     }
-    const wb = xlsx.utils.book_new();
-    const ws = xlsx.utils.aoa_to_sheet(input);
-
-    xlsx.utils.book_append_sheet(wb, ws, "Workout Data");
-
-    // Can choose excel or CSV here
-    const excelBuffer = xlsx.write(wb, { bookType: "xlsx", type: "buffer" });
-
-    const headers = new Headers();
-    headers.append(
-      "Content-Type",
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    );
-    headers.append(
-      "Content-Disposition",
-      "attachment; filename=user_data.xlsx"
-    );
-
-    const response = new Response(excelBuffer, {
-      status: 200,
-      headers: headers,
-    });
-
-    return response;
-  } else {
-    console.log("no user found :(");
+    input.push([]);
+    input.push([]);
   }
-  return new Response("Exporting data");
+  const wb = xlsx.utils.book_new();
+  const ws = xlsx.utils.aoa_to_sheet(input);
+
+  xlsx.utils.book_append_sheet(wb, ws, "Workout Data");
+
+  // Can choose excel or CSV here
+  const excelBuffer = xlsx.write(wb, { bookType: "xlsx", type: "buffer" });
+
+  const headers = new Headers();
+  headers.append(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  );
+  headers.append("Content-Disposition", "attachment; filename=user_data.xlsx");
+
+  const response = new Response(excelBuffer, {
+    status: 200,
+    headers: headers,
+  });
+
+  return response;
 }
