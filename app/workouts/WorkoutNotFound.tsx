23a7970e07ef436ef01f1feac780fa397eaad76d
Kevin Tatooles <kevintatooles@gmail.com>
2025-01-15 19:03:13 -0600

    Remove unused flex-col

--- app/workouts/WorkoutNotFound.tsx
+++ app/workouts/WorkoutNotFound.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function WorkoutNotFound() {
   return (
-    <div className="mx-auto flex-col p-4 text-center sm:max-w-md">
+    <div className="mx-auto p-4 text-center sm:max-w-md">
       <p className="text-left mb-4">
         Workout not found, please return to the Workouts page
       </p>
