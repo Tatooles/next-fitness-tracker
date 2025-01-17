d0b3d7adbe3bf37e0dff810317c60ad33b37b74f
Kevin Tatooles <kevintatooles@gmail.com>
2025-01-16 22:41:02 -0600

    Template UI for 1rm calculation

--- components/ExerciseSummaryComponent.tsx
+++ components/ExerciseSummaryComponent.tsx
@@ -6,9 +6,7 @@ export default function ExerciseSummaryComponent({
 }: {
   exerciseSummary: ExerciseSummary;
 }) {
-  // Gotta run some calculations
-  // I guess they will be done on the client
-  const maxWeightLifted = (): number => {
+  const calculateWeightLifted = (): number => {
     let maxWeight = 0;
 
     exerciseSummary.exercises.forEach((exercise) => {
@@ -19,13 +17,28 @@ export default function ExerciseSummaryComponent({
 
     return maxWeight;
   };
+
+  const calculateOneRepMax = (): number => {
+    // Calculating max of every possible set seems excessive
+    // But can't think of another way
+    return 0;
+  };
+
   return (
     <div className="flex flex-col gap-2">
-      <Button className="self-start">History</Button>
-      <span className="self-start text-xl">
-        Heaviest rep {maxWeightLifted()}
-      </span>
-      <span className="self-start text-xl">Calculated 1RM {}</span>
+      <Button className="self-start text-lg">History</Button>
+      <div className="self-start text-xl mb-4">
+        <span className="mr-4">Heaviest rep</span>
+        <span className="p-2 rounded-md  bg-amber-300">
+          {calculateWeightLifted()} lb
+        </span>
+      </div>
+      <div className="self-start text-xl">
+        <span className="mr-4">Calculated 1RM</span>
+        <span className="p-2 rounded-md  bg-amber-300">
+          {calculateOneRepMax()} lb
+        </span>
+      </div>
     </div>
   );
 }
