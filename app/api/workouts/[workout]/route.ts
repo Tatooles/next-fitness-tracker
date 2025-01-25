e64326ece8c4e25b4af91451c330574b8764554c
Kevin Tatooles <kevintatooles@gmail.com>
2025-01-25 00:03:58 -0600

    Clean up iterative delete logic

--- app/api/workouts/[workout]/route.ts
+++ app/api/workouts/[workout]/route.ts
@@ -1,42 +1,13 @@
 import { eq } from "drizzle-orm";
 import { db } from "@/db/drizzle";
 import { workouts } from "@/db/schema";
-import { exercises } from "@/db/schema";
-import { sets } from "@/db/schema";
 
 export async function DELETE(
   request: Request,
-  { params }: { params: Promise<{ workout: number }> }
+  { params }: { params: Promise<{ id: number }> }
 ) {
-  const workout = (await params).workout;
   try {
-    await db.transaction(async (tx) => {
-      const result = await db.query.workouts.findMany({
-        where: (workouts, { eq }) => eq(workouts.id, workout),
-        columns: {},
-        with: {
-          exercises: {
-            columns: {
-              id: true,
-            },
-            with: {
-              sets: {
-                columns: {
-                  id: true,
-                },
-              },
-            },
-          },
-        },
-      });
-      for (const exercise of result[0].exercises) {
-        await db.delete(exercises).where(eq(exercises.id, exercise.id));
-        for (const set of exercise.sets) {
-          await db.delete(sets).where(eq(sets.id, set.id));
-        }
-      }
-      await db.delete(workouts).where(eq(workouts.id, workout));
-    });
+    await db.delete(workouts).where(eq(workouts.id, (await params).id));
     return new Response("Workout successfully deleted");
   } catch (error) {
     console.log("An error ocurred!");
