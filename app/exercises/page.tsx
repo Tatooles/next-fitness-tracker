cc34c58628a7f1aca2119f51d57b33ceaa746740
Kevin Tatooles <kevintatooles@gmail.com>
2025-01-26 17:59:55 -0600

    Update query and type conversion logic

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,59 +1,80 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/db/drizzle";
 import ExercisesUI from "./ExercisesUI";
-import { DateExercise, ExerciseSummary, Workout } from "@/lib/types";
+import { ExerciseInstance, Set } from "@/lib/types";
+import { exerciseView, sets } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+interface DateExercise extends ExerciseInstance {
+  date: string;
+  userId: string;
+}
+
+type ExerciseWithSets = {
+  userId?: string | null;
+  date?: string | null;
+  id?: number | null;
+  name?: string | null;
+  notes?: string | null;
+  workoutId?: number | null;
+  sets: Set[];
+};
+
+export interface ExerciseViewData {
+  userId?: string | null;
+  date?: string | null;
+  id?: number | null;
+  name?: string | null;
+  notes?: string | null;
+  workoutId?: number | null;
+  sets: Set[];
+}
 
 async function getExercises() {
   try {
     const { userId, redirectToSignIn } = await auth();
     if (!userId) redirectToSignIn();
 
-    const data = await db.query.workouts.findMany({
-      where: (workouts, { eq }) => eq(workouts.userId, userId!),
-      orderBy: (workouts, { desc }) => [desc(workouts.date)],
-      columns: {
-        date: true,
-      },
-      with: {
-        exercises: {
-          with: {
-            sets: {
-              orderBy: (sets, { asc }) => [asc(sets.id)],
-            },
-          },
-        },
-      },
-    });
+    // Maybe add type for data so everything can't be null
+    const data = await db
+      .select()
+      .from(exerciseView)
+      .fullJoin(sets, eq(sets.exerciseId, exerciseView.id))
+      .where(eq(exerciseView.userId, userId!));
+    // TODO: Group by name??
+    // Otherwise have to do it later
 
     return data;
   } catch (error) {
-    console.log("An error ocurred while fetching workout data");
+    console.log("An error ocurred while fetching workout data", error);
     return [];
   }
 }
 
-async function getExerciseSummary(): Promise<ExerciseSummary[]> {
-  const workouts = (await getExercises()) as Workout[];
+async function getExerciseSummary() {
+  const exercises = await getExercises();
 
-  const exercises = workouts.flatMap((workout) =>
-    workout.exercises.map((exercise) => ({
-      ...exercise,
-      date: workout.date,
-    }))
-  ) as DateExercise[];
+  const groupedResults = exercises.reduce<ExerciseViewData[]>(
+    (acc, { exercise_view, sets }) => {
+      const existingExercise = acc.find((e) => e.id === exercise_view?.id);
+      if (existingExercise) {
+        if (sets) {
+          existingExercise.sets.push(sets);
+        }
+      } else {
+        if (sets) {
+          acc.push({
+            ...exercise_view,
+            sets: [sets],
+          });
+        }
+      }
+      return acc;
+    },
+    []
+  );
 
-  const grouped = exercises.reduce((acc, exercise) => {
-    if (!acc[exercise.name]) {
-      acc[exercise.name] = [];
-    }
-    acc[exercise.name].push(exercise);
-    return acc;
-  }, {} as Record<string, DateExercise[]>);
-
-  return Object.entries(grouped).map(([name, exerciseGroup]) => ({
-    name,
-    exercises: exerciseGroup,
-  }));
+  console.log(groupedResults[0]);
 }
 
 export default async function ExercisesPage() {
