2202f19b0e2d1711793e5ee8f5348f46d0a28391
Kevin Tatooles <kevintatooles@gmail.com>
2025-01-30 22:43:40 -0600

    Add RPE to history UI

--- components/ExerciseInstanceItem.tsx
+++ components/ExerciseInstanceItem.tsx
@@ -10,31 +10,35 @@ export default function ExerciseInstanceItem({
   showDate: boolean;
 }) {
   return (
-    <div className="p-2">
+    <div className="p-2 mt-4">
       {showName && <h3 className="text-lg font-bold">{exercise.name}</h3>}
       {showDate && <h3 className="text-lg text-center">{exercise.date}</h3>}
-      {exercise.sets.length > 0 && (
-        // Could have global state (set in settings) to determine if this
-        // has other columns like RPE, would need changes in the input modal too
-        <div className="flex justify-around">
-          <div>Reps</div>
-          <div>Weight</div>
-        </div>
-      )}
-      {exercise.sets.map(
-        (set: Set, index) =>
-          (set.reps || set.weight) && (
-            <div key={index} className="flex justify-around">
-              <div>{set.reps}</div>
-              <div>{set.weight}</div>
-            </div>
-          )
-      )}
-      {exercise.notes && (
-        <p className="mt-2 rounded-md dark:text-black bg-slate-300 p-2">
-          {exercise.notes}
-        </p>
-      )}
+      <div className="mt-4">
+        {exercise.sets.length > 0 && (
+          // Could have global state (set in settings) to determine if this
+          // has other columns like RPE, would need changes in the input modal too
+          <div className="flex justify-around">
+            <div>Reps</div>
+            <div>Weight</div>
+            <div>RPE</div>
+          </div>
+        )}
+        {exercise.sets.map(
+          (set: Set, index) =>
+            (set.reps || set.weight) && (
+              <div key={index} className="flex justify-around">
+                <div>{set.reps}</div>
+                <div>{set.weight}</div>
+                <div>{set.rpe}</div>
+              </div>
+            )
+        )}
+        {exercise.notes && (
+          <p className="mt-2 rounded-md dark:text-black bg-slate-300 p-2">
+            {exercise.notes}
+          </p>
+        )}
+      </div>
     </div>
   );
 }
