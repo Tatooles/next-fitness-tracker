c0a0a2d19d9c479993c41801fcc7ccfe369899d2
Kevin Tatooles <kevintatooles@gmail.com>
2025-02-19 21:39:01 -0600

    Create proper skeleton for workouts page

--- app/workouts/loading.tsx
+++ app/workouts/loading.tsx
@@ -1,6 +1,13 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <Skeleton />;
+export default function WorkoutsLoading() {
+  return (
+    <div className="flex flex-col items-center p-5 gap-5">
+      <Skeleton className="h-10 w-32" />
+      <Skeleton className="h-10 w-32" />
+      {[...Array(8)].map(() => (
+        <Skeleton className="h-10 w-full" />
+      ))}
+    </div>
+  );
 }
