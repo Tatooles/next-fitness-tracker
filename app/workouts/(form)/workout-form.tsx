ca06c9068228fce8924cfdd8eb09babf64862c6f
Kevin Tatooles <kevintatooles@gmail.com>
2025-02-19 21:57:09 -0600

    Move form related workout files into route group for organization and shared loading.tsx

--- app/workouts/(form)/workout-form.tsx
+++ app/workouts/(form)/workout-form.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import FormSets from "@/app/workouts/form-sets";
+import FormSets from "@/app/workouts/(form)/form-sets";
 import Spinner from "@/components/spinner";
 import { workoutFormSchema, TWorkoutFormSchema } from "@/lib/types";
 import {
