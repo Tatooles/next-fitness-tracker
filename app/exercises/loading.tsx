8f4e3c14a097a86e6cf1b110968473a81ab24510
Kevin Tatooles <kevintatooles@gmail.com>
2025-02-19 22:17:41 -0600

    Fix lint errors to fix build

--- app/exercises/loading.tsx
+++ app/exercises/loading.tsx
@@ -5,8 +5,8 @@ export default function ExercisesLoading() {
     <div className="flex flex-col items-center p-5 gap-5">
       <Skeleton className="h-10 w-32" />
       <Skeleton className="h-8 w-full" />
-      {[...Array(8)].map(() => (
-        <Skeleton className="h-9 w-full" />
+      {[...Array(8)].map((_, index) => (
+        <Skeleton key={index} className="h-9 w-full" />
       ))}
     </div>
   );
--- app/workouts/loading.tsx
+++ app/workouts/loading.tsx
@@ -5,8 +5,8 @@ export default function WorkoutsLoading() {
     <div className="flex flex-col items-center p-5 gap-5">
       <Skeleton className="h-10 w-32" />
       <Skeleton className="h-10 w-32" />
-      {[...Array(8)].map(() => (
-        <Skeleton className="h-10 w-full" />
+      {[...Array(8)].map((_, index) => (
+        <Skeleton key={index} className="h-10 w-full" />
       ))}
     </div>
   );
