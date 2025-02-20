4fa9f0dd5e1b7a24f1f9f5ca92b48e593f570001
Kevin Tatooles <kevintatooles@gmail.com>
2025-02-19 21:51:34 -0600

    Add loading skeletons for exercises page and create workout page

---
+++ app/exercises/loading.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function ExercisesLoading() {
+  return (
+    <div className="flex flex-col items-center p-5 gap-5">
+      <Skeleton className="h-10 w-32" />
+      <Skeleton className="h-8 w-full" />
+      {[...Array(8)].map(() => (
+        <Skeleton className="h-9 w-full" />
+      ))}
+    </div>
+  );
+}
