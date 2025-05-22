4d71fa3a64d53f58112422d4ff94b46499128384
Kevin Tatooles <kevintatooles@gmail.com>
2025-05-21 21:37:13 -0500

    Update home page text

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,30 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-background text-foreground py-12 px-4 sm:px-6 lg:px-8 text-center">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-background text-foreground p-4 sm:px-6 lg:px-8 text-center">
       <h1 className="text-4xl sm:text-5xl font-extrabold mb-8 sm:mb-12 tracking-tight">
-        Welcome to the Lifting Log!
+        Track Your Strength Journey
       </h1>
       <div className="flex flex-col justify-center gap-6 sm:gap-8 sm:flex-row">
         <div className="bg-card text-card-foreground rounded-xl shadow-xl p-6 sm:p-8 max-w-md transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <h2 className="text-2xl sm:text-3xl font-semibold mb-6">Workouts</h2>
+          <h2 className="text-2xl sm:text-3xl font-semibold mb-6">
+            Log Your Workouts
+          </h2>
           <p className="mb-8 text-muted-foreground">
-            Create, view and edit your workouts. Log all your sets and reps, as
-            well as any notes you have about your workout.
+            Effortlessly create, view, and manage your training sessions. Record
+            every set, rep, and note to stay on top of your game.
           </p>
           <Button asChild size="lg" className="w-full sm:w-auto">
             <Link href="/workouts">View Workouts</Link>
           </Button>
         </div>
         <div className="bg-card text-card-foreground rounded-xl shadow-xl p-6 sm:p-8 max-w-md transform hover:scale-105 transition-transform duration-300 ease-in-out">
-          <h2 className="text-2xl sm:text-3xl font-semibold mb-6">Exercises</h2>
+          <h2 className="text-2xl sm:text-3xl font-semibold mb-6">
+            Analyze Your Progress
+          </h2>
           <p className="mb-8 text-muted-foreground">
-            See all your exercises in one place, compare the same exercise
-            across different days to see your progress.
+            Dive into your exercise history. Compare performances, visualize
+            your strength gains, and stay motivated.
           </p>
           <Button asChild size="lg" className="w-full sm:w-auto">
             <Link href="/exercises">View Exercises</Link>
