bdddd32213599bee542989b1a1105e12fb306586
Kevin Tatooles <kevintatooles@gmail.com>
2025-12-27 13:34:46 -0600

    Initialize today's date in workout form if not provided

--- app/workouts/(form)/create/page.tsx
+++ app/workouts/(form)/create/page.tsx
@@ -1,14 +1,12 @@
 import WorkoutForm from "@/app/workouts/(form)/workout-form";
 
-export const dynamic = "force-dynamic";
-
 export default async function CreateWorkoutPage() {
   return (
     <WorkoutForm
       editMode={false}
       workoutId={-1}
       workoutValue={{
-        date: new Date().toLocaleDateString("en-CA"),
+        date: "",
         name: "",
         exercises: [
           { name: "", notes: "", sets: [{ weight: "", reps: "", rpe: "" }] },
