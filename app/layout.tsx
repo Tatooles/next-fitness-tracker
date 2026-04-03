d6186d26c85698314eedafbedfda6a51a8fd221d
Kevin Tatooles <kevintatooles@gmail.com>
2026-04-03 17:16:42 -0500

    Reformat layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
           </ThemeProvider>
           <SpeedInsights />
         </ClerkProvider>
-        </body>
-      </html>
+      </body>
+    </html>
   );
 }
