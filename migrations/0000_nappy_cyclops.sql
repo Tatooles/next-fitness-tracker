8cf295cdd3ffb99bc1e2c85eb04b497f87e8570e
Kevin Tatooles <kevintatooles@gmail.com>
2026-04-16 19:40:22 -0500

    Add migrations and github action to run them

---
+++ migrations/0000_nappy_cyclops.sql
@@ -0,0 +1,8 @@
+CREATE INDEX IF NOT EXISTS `workout_user_date_idx`
+  ON `workout` (`user_id`,`date`);
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `exercise_workout_name_idx`
+  ON `exercise` (`workout_id`,`name`);
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `set_exercise_id_id_idx`
+  ON `set` (`exercise_id`,`id`);
